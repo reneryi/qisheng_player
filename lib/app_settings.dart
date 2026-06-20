08547139ac19704edc94dfa6efbbbcb7cca22012
reneryi <reneryi@users.noreply.github.com>
2026-06-20 15:55:14 +0800

    chore: bump version to 1.2.8

--- lib/app_settings.dart
+++ lib/app_settings.dart
@@ -90,7 +90,7 @@ Future<Directory> getAppDataDir() async {
 
 class AppSettings {
   static final github = GitHub();
-  static const String version = "1.2.7";
+  static const String version = "1.2.8";
   static const String releaseRepoOwner = "reneryi";
   static const String releaseRepoName = "qisheng_player";
 
